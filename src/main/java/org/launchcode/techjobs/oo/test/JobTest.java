f00b6b18e4d9b1a3dca1b97fc55528a1ead0a7e7
reshmanarute <reshma.narute@gmail.com>
2021-10-03 17:49:58 -0400

    Moved back to src

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.*;
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
-public class JobTest{
+public class JobTest {
     private Job firstJob, secondJob, thirdJob, fourthJob, fifthJob;
 
     @Before
